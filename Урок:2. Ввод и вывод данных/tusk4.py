a6699ad8752a4498b1b28d606ffad2ad939cca48
FedorovBair <167606625+FedorovBair@users.noreply.github.com>
2024-04-20 15:41:53 +0800

    Add files via upload

---
+++ Урок:2. Ввод и вывод данных/tusk4.py
@@ -0,0 +1,3 @@
+# Задание 4
+for i in range(1, 6):
+    print('*' * i)
---
+++ Урок:2. Ввод и вывод данных/tusk6.py
@@ -0,0 +1,7 @@
+# Задание 6
+string1 = input("Введите первую строку: ")
+string2 = input("Введите вторую строку: ")
+string3 = input("Введите третью строку: ")
+print(string3)
+print(string2)
+print(string1)
